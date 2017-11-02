c3111f56888f2d31a6154df755ff28dc9fc97ccc
redlion47 <katoo.eric@gmail.com>
2017-11-02 17:12:01 +0300

    [Fix] fixed Procfile to enable deployment on heroku and ignored pycache files

--- app/category.py
+++ app/category.py
@@ -5,6 +5,9 @@ class Category:
         selt.title = title
 
     def addCategory(title):
+        """The method is for creating and
+        storing a category"""
+
         if title == '':
             return {'status': False, 'message': "Please enter a title. We dont expect an empty String"}
 
@@ -16,8 +19,15 @@ class Category:
             return {'status': True, 'categories': cats}
 
     def getCategories():
+        """The method to get a list of all
+        the categories availabe"""
 
-        return cats
+        categoryList = []
+
+        for category in cats:
+            categoryList.append(category['title'])
+
+        return categoryList #a list of categories
 
 class recipe(Category):
 
@@ -28,17 +38,48 @@ class recipe(Category):
         self.instructions = instructions
 
     def joinRecipeToCategory(catTitle,recipe, ingredients, instructions):
-        pickedCategory  = cats[catTitle]
-        recipes = {}
-        recipes['recipe'] = recipe
-        recipes['ingredients'] = ingredients
-        recipes['instructions'] = instructions
+        """This category associates 
+        the recipes the user adds
+        with the category selected"""
 
-        for categ in cats:
-            if categ[catTitle]:
-                return categ
+        if catTitle != "":
+            if recipe != "" and (ingredients != "" or instructions != ""):
+                
+                recipes = {}
+                recipes['recipe'] = recipe
+                recipes['ingredients'] = ingredients
+                recipes['instructions'] = instructions
+
+                for categ in cats:
+                    for key, value in categ.items():
+                        if value == catTitle:
+                            categ[recipe] = recipes
+
+                        else:
+                            break
+
+                cats.append(categ)
+                message = "Recipe successfully added!!!!"
+
+                return {'status' : True, 'message': message, 'categoryRecipes' : cats }
+
+            else:
+                message = "You can't leave a recipe empty. Can you cook water??"
+                return {'status' : False, 'message' : message}
+
+        else:
+            return {'status': False, 'message' : "Please provide a category!"}
 
 
-
-
+    def deletionFromStore(self):
+        """This method is for enabling deletion
+        of both categories and recipes from our
+        storage point"""
         
+        pass
+
+    def getRecipeList(self):
+        """This method will be used to get a 
+        list of all the recipes associated 
+        with a category"""
+        pass
